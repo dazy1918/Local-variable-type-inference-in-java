fa51dd8d042afcdb82e1e924da4ad9d9ceadc379
Dazy Baliyan <baliyandazy22@gmail.com>
2026-02-10 09:52:02 +0530

    Add files via upload

--- localvariable1.java
+++ localvariable1.java
@@ -1,11 +1,11 @@
 class Test2 {
     Test2() {
-        var x = 23;   // local variable in constructor
-        System.out.println("constructor value is " + x);
+        var x = 80;   // local variable in constructor
+        System.out.println("Constructor value is " + x);
     }
 
     void check() {
-        var s = "hello";   // local variable in method
+        var s = "MCA";   // local variable in method
         System.out.println(s);
     }
 }
@@ -13,6 +13,9 @@ class Test2 {
 public class localvariable1 {
     public static void main(String[] args) {
         Test2 t = new Test2(); 
-        t.check();            // method call
+        t.check();            
+        for(var i=0; i<=4; i++){    //Inside a block
+            System.out.println(i);
+        }
     }
 }
