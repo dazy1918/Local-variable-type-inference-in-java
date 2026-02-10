ea2e5e4f03735e1ecade116e04a741e89b3b61dc
Dazy Baliyan <baliyandazy22@gmail.com>
2026-02-10 09:58:54 +0530

    Add files via upload

---
+++ textblock.java
@@ -0,0 +1,10 @@
+public class textblock {
+    public static void main(String[] args) {
+       String s ="""
+                 11/2,
+                 kiet University,
+                 Ghaziabad
+                 """;
+      System.out.println(s);                         
+    }
+}
